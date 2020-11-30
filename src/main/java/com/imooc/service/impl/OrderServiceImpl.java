d7307ae94c75c09bc6cca3891c42b2a01fcd8b74
mowen <963774034@qq.com>
2020-11-30 19:09:43 +0800

    买家订单service-2（完成，包括test）

--- src/main/java/com/imooc/service/impl/OrderServiceImpl.java
+++ src/main/java/com/imooc/service/impl/OrderServiceImpl.java
@@ -3,7 +3,10 @@ package com.imooc.service.impl;
 import com.imooc.dataobject.OrderDetail;
 import com.imooc.dataobject.OrderMaster;
 import com.imooc.dataobject.ProductInfo;
+import com.imooc.dto.CartDTO;
 import com.imooc.dto.OrderDTO;
+import com.imooc.enums.OrderStatusEnum;
+import com.imooc.enums.PayStatusEnum;
 import com.imooc.enums.ResultEnum;
 import com.imooc.exception.SellException;
 import com.imooc.repository.OrderDetailRepository;
@@ -16,10 +19,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.awt.print.Pageable;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.security.Key;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -34,10 +41,12 @@ public class OrderServiceImpl implements OrderService {
     private OrderMasterRepository orderMasterRepository;
 
     @Override
+    @Transactional
     public OrderDTO create(OrderDTO orderDTO) {
 
         String orderId = KeyUtil.genUniqueKey();
         BigDecimal orderAmount = new BigDecimal(BigInteger.ZERO);
+        // List<CartDTO> cartDTOList = new ArrayList<>();
 
         //1.查询商品（数量、价格）
         for (OrderDetail orderDetail: orderDTO.getOrderDetailList()) {
@@ -48,7 +57,7 @@ public class OrderServiceImpl implements OrderService {
             // 判断数量是否足够？不需要，可以到扣库存做
 
             //2.计算订单总价
-            orderDetail.getProductPrice()
+            orderAmount = productInfo.getProductPrice()
                     .multiply(new BigDecimal(orderDetail.getProductQuantity()))
                     .add(orderAmount);
 
@@ -57,18 +66,28 @@ public class OrderServiceImpl implements OrderService {
             orderDetail.setOrderId(orderId);
             BeanUtils.copyProperties(productInfo, orderDetail);
             orderDetailRepository.save(orderDetail);
+
+            // CartDTO cartDTO = new CartDTO(orderDetail.getProductId(), orderDetail.getProductQuantity());
+            // cartDTOList.add(cartDTO);
         }
 
         //3.写入订单数据库（orderMaster和orderDetail）
         OrderMaster orderMaster = new OrderMaster();
+        BeanUtils.copyProperties(orderDTO, orderMaster);
         orderMaster.setOrderId(orderId);
         orderMaster.setOrderAmount(orderAmount);
-        BeanUtils.copyProperties(orderDTO, orderMaster);
+        orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
+        orderMaster.setPayStatus(PayStatusEnum.WAIT.getCode());
         orderMasterRepository.save(orderMaster);
 
         //4.扣库存
+        List<CartDTO> cartDTOList = orderDTO.getOrderDetailList().stream().map(e->
+                new CartDTO(e.getProductId(), e.getProductQuantity())
+        ).collect(Collectors.toList());
 
-        return null;
+        productService.decreaseStock(cartDTOList);
+
+        return orderDTO;
     }
 
     @Override
