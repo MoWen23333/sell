d30edc5558eb1df5327db9467dd31d3ce0c6602b
mowen <963774034@qq.com>
2020-12-05 17:31:36 +0800

    买家订单-第三方sdk授权

--- src/main/java/com/imooc/controller/WeixinController.java
+++ src/main/java/com/imooc/controller/WeixinController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+/* 手工方式 */
 @RestController
 @RequestMapping("/weixin")
 @Slf4j
