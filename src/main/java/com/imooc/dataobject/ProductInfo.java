a088485c63869be7ed80273ce3d40bc6976aa550
MoWen <963774034@qq.com>
2020-12-14 20:41:57 +0800

    卖家端-商品列表

--- src/main/java/com/imooc/dataobject/ProductInfo.java
+++ src/main/java/com/imooc/dataobject/ProductInfo.java
@@ -1,16 +1,22 @@
 package com.imooc.dataobject;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.imooc.enums.ProductStatusEnum;
+import com.imooc.utils.EnumUtil;
 import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  * 商品
  */
 @Entity
 @Data
+@DynamicUpdate
 public class ProductInfo {
 
     @Id
@@ -36,5 +42,16 @@ public class ProductInfo {
 
     /** 类目编号. */
     private Integer categoryType;
+
+    /** 创建时间. */
+    private Date createTime;
+
+    /** 修改时间. */
+    private Date updateTime;
+
+    @JsonIgnore
+    public ProductStatusEnum getProductStatusEnum() {
+        return EnumUtil.getByCode(productStatus, ProductStatusEnum.class);
+    }
 
 }
